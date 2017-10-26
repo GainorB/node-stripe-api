ca475035afd7c3e28a166e5104cd88860b90f36f
GainorB <gainorbostwick@gmail.com>
2017-10-26 01:23:49 -0400

    routes, css, handlebars setup, bootstrap

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 require('dotenv').config();
 const express = require('express');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 // INITIALIZE EXPRESS
 const app = express();
 
 // VIEW ENGINE SETUP
-app.set('view engine', 'ejs');
+// HANDLEBARS
 
-const PORT = 3000;
+// THE LAYOUT THAT WRAPS AROUND ALL OUR VIEWS IS CALLED MAIN.HANDLEBARS
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// MIDDLEWARE
+
+// BODYPARSER
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// STATIC FOLDER
+app.use(express.static(path.join(__dirname, 'public')));
+
+// INDEX ROUTE
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// START SERVER
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
